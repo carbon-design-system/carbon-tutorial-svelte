30c60f0312bd02f620dd9bb4ee4b3ac06f8f63e7
Eric Liu <ericyl.us@gmail.com>
2021-05-11 15:39:38 -0700

    feat: reset step 1

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
 import adapter from "@sveltejs/adapter-static";
-import * as carbon from "carbon-preprocess-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-  preprocess: carbon.presetCarbon(),
   kit: {
     target: "#svelte",
     adapter: adapter(),
-    vite: {
-      optimizeDeps: { include: ["clipboard-copy"] },
-      plugins: [process.env.NODE_ENV === "production" && carbon.optimizeCss()],
-    },
   },
 };
